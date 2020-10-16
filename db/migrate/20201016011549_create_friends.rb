a0a8f64262666cf9a43d9a2a40b5a0c56fb6d374
Lydia Gregory <ljg2gb@virginia.edu>
2020-10-15 21:17:27 -0400

    adds Friends resources and sets model relationships

---
+++ db/migrate/20201016011549_create_friends.rb
@@ -0,0 +1,11 @@
+class CreateFriends < ActiveRecord::Migration[6.0]
+  def change
+    create_table :friends do |t|
+      t.string :name
+      t.date :dob
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
