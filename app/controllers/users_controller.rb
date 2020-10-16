20c1f89b1262cc0a22f0306e2c1702be5c0abb74
Lydia Gregory <ljg2gb@virginia.edu>
2020-10-15 21:12:50 -0400

    creates functional create action in User's conttroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
     def create
+        @user = User.new(user_params)
+
+            if @user.save
+                token = create_token(@user)
+                render json: {
+                    message: "Success user #{@user.name} has been created.",
+                    token: token
+                }
+            else
+                render json: { message: @user.errors.messages }
+            end
     end
 
     private
